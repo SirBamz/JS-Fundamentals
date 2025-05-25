49dfafb0168d705e15ffe4ff569bf36f2bd4eeb1
Bamikiya Precious Ayomide <133870138+SirBamz@users.noreply.github.com>
2025-05-26 00:21:31 +0100

    Add files via upload

---
+++ 8-square.js
@@ -0,0 +1,14 @@
+const sizeArg = process.argv[2];
+const size = parseInt(sizeArg);
+
+if (isNaN(size)) {
+  console.log("Missing size");
+} else {
+  for (let i = 0; i < size; i++) {
+    let line = "";
+    for (let j = 0; j < size; j++) {
+      line += "X";
+    }
+    console.log(line);
+  }
+}
